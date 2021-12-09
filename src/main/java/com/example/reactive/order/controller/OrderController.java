73df71f6b28354e2d8ae57bbea7be7168d80b6c6
Marimuthu <mmuthu.mtech@gmail.com>
2021-12-09 12:22:51 +0530

    updated changes on order service with kafka consumer and producer

--- src/main/java/com/example/reactive/order/controller/OrderController.java
+++ src/main/java/com/example/reactive/order/controller/OrderController.java
@@ -4,11 +4,8 @@ import java.time.Duration;
 import java.util.Date;
 import java.util.stream.Stream;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.kafka.core.KafkaTemplate;
 //import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.reactive.order.model.Order;
 import com.example.reactive.order.model.OrderEvent;
+import com.example.reactive.order.producer.KafkaEventProducer;
 import com.example.reactive.order.repository.OrderRepository;
 
 import lombok.extern.slf4j.Slf4j;
@@ -29,82 +27,59 @@ import reactor.util.function.Tuple2;
 @RestController
 @RequestMapping("/order")
 
-//private Logger log = LoggerFactory.getlogger(OrderController.class);
 @Slf4j
 public class OrderController {
-	
-//	@Autowired
-//	KafkaTemplate<String, Order> kafkaTemplate;
-////	
 	@Autowired
-	KafkaTemplate<String, String> kafkaStrTemplate;
-	
-	// spring web flux Mono is equivalent to CompletableFuture non blocking callback operation
+	KafkaEventProducer kafkaEventProducer;
+	// spring web flux Mono is equivalent to CompletableFuture non blocking callback
+	// operation
 	// Mono 0..1 elements
 	// Flux 0 .. N elements
-	
+
 	private OrderRepository orderRepository;
 
-	private Logger log=LoggerFactory.getLogger(OrderController.class);
-	private static final String TOPIC_NAME = "test_order";
 	public OrderController(OrderRepository orderRepository) {
-		
+
 		this.orderRepository = orderRepository;
 	}
+
 	// Flux 0 .. N elements ... list of values
 	@GetMapping("/all")
-	public Flux<Order> getAll(){
-		log.info("order service getAll( api invoked");
-		System.out.println("getAll API called");
+	public Flux<Order> getAll() {
+		log.info("order service getAll api invoked");
 		return orderRepository.findAll();
 	}
+
 	// Mono 0..1 elements ... single value
 	@GetMapping("/{id}")
-	public Mono<Order> getOrder(@PathVariable("id") final String orderId){
+	public Mono<Order> getOrder(@PathVariable("id") final String orderId) {
 		log.info("order service getOrder( api invoked");
-		System.out.println("getOrder API called");
 		return orderRepository.findById(orderId);
 	}
+
 	// Flux 0 .. N elements .... live list of values
-	@GetMapping(value="/events", produces=MediaType.TEXT_EVENT_STREAM_VALUE)
-	public Flux<OrderEvent> getOrderEvents(){
-		System.out.println("getOrderEvents API Called");
-//		Mono<Integer> a = Mono.just(1);
-//		Integer b = a.block();
-//		System.out.println("b === " + a);
-		return orderRepository.findAll()
-		.flatMap(order -> {
+	@GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<OrderEvent> getOrderEvents() {
+		log.info("getOrderEvents API Called");
+		return orderRepository.findAll().flatMap(order -> {
 			Flux<Long> interval = Flux.interval(Duration.ofSeconds(2));
-			Flux<OrderEvent> employeeEventFlux = 
-					Flux.fromStream(Stream.generate(
-							() -> new OrderEvent(order, new Date())));
-			return Flux.zip(interval, employeeEventFlux)
-			.map(Tuple2::getT2)
-			.filter(p -> p.getOrder().getOrderName().contains("Marimuthu"));
+			Flux<OrderEvent> employeeEventFlux = Flux
+					.fromStream(Stream.generate(() -> new OrderEvent(order, new Date())));
+			return Flux.zip(interval, employeeEventFlux).map(Tuple2::getT2)
+					.filter(p -> p.getOrder().getOrderName().contains("Marimuthu"));
 		});
-		
+
 	}
-//	@PostMapping("/publish")
-//	public void postOrderEvent(@RequestBody Order order) {
-//		System.out.println("postOrderEvent API called");
-//		try {
-//			kafkaTemplate.send(TOPIC_NAME , order);
-//			System.out.println("Event published sucessfully...");
-//		} catch(Exception e) {
-//			System.out.println("failed to publish kafka msg, Exception msg is " + e.getMessage());
-//		}
-//		
+	@PostMapping("/eventMessage")
+	public void publishEventMessage(@RequestBody Order order) {
+		log.info("publishEventMessage API invoked...");
+		kafkaEventProducer.publishEvent(order);
+	}
+
+//	@PostMapping("/eventMessage/{msg}")
+//	public void publishString(@PathVariable("msg") String msg) {
+//		kafkaEventProducer.publishMessage(msg);
+//		log.info("Kafka message published successfully...");
 //	}
-	@PostMapping("/publishString/{msg}")
-	public void publishString(@PathVariable("msg") String msg) {
-		System.out.println("publishString API called");
-		try {
-			System.out.println("Msg recieved " + msg);
-			kafkaStrTemplate.send(TOPIC_NAME , msg);
-			System.out.println("Event published sucessfully...");
-		} catch(Exception e) {
-			System.out.println("failed to publish kafka msg, Exception msg is " + e.getMessage());
-		}
-		
-	}
-	}
+
+}
